4b24f41b27d0738da4e44b0c5ac01db86f328856
thanhtutoo <5531305090@lamduan.mfu.ac.th>
2018-11-25 21:57:34 +0630

    added userheader

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch =>{
@@ -9,4 +10,10 @@ export const fetchPosts = () => async dispatch =>{
         //     payload:promise
         // }
 
+};
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type:'FETCH_USER', payload:response.data});
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component{
     componentDidMount(){
@@ -15,6 +16,9 @@ class PostList extends React.Component{
                         <div className="description">
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
+                        </div>
+                        <div className="">
+                            <UserHeader userId= {post.userId} />
                         </div>
                     </div>
                 </div>
